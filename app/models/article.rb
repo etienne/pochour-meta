8c5f2d8c60baf25d17ee0dbcc982ff03f774a0f8
Étienne Després <etienne@pochour.com>
2015-09-13 21:17:42 -0400

    Don't show comment-less articles in recent comments.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
   belongs_to :community
   has_many :comments, -> { order "created_at" }
-  scope :recently_commented, -> { order("last_comment_at DESC").limit(12) }
+  scope :recently_commented, -> { where.not(last_comment_at: nil).order("last_comment_at DESC").limit(12) }
   
   def update_comment_metadata
     update!(comment_count: comments.size, last_comment_at: comments.last.created_at)
