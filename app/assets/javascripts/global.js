d235fbe37db2477d4fe9787640f40e5904628cd7
Étienne Després <etienne@pochour.com>
2015-09-20 23:03:19 -0400

    Remove header.js.

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,4 +1,9 @@
 $(function() {
+  // Fade out notices
+  setTimeout(function () {
+    $('div.notice').fadeOut(1000);
+  }, 1500);
+  
   // Activate in-place editor
   $(".best_in_place").best_in_place();
   
@@ -22,4 +27,38 @@ $(function() {
       });
     });
   }
+  
+  // Handle popup menus
+  $('.popup-menu-link').mousedown(function() {
+    $('.popup-menu').fadeOut(100);
+    var popup_menu = $('#' + $(this).attr('data-popup'));
+    popup_menu.fadeToggle(100);
+    
+    if (popup_menu.is(':visible')) {
+      $(popup_menu).css({
+        transform: "translate(0, 0)"
+      })
+      
+      var calculator = new $.PositionCalculator({
+        item: popup_menu,
+        itemAt: "top center",
+        target: this,
+        targetAt: "bottom center",
+        flip: "both"
+      });
+      var position = calculator.calculate();
+
+      popup_menu.css({
+        transform: "translate(" + position.moveBy.x + "px, " + (position.moveBy.y + 14) + "px)"
+      })
+    }
+  });
+  
+  $(document).mousedown(function(event) {
+    if (!$(event.target).closest('.popup-menu, .popup-menu-link').length) {
+      if ($('.popup-menu').is(":visible")) {
+        $('.popup-menu').fadeOut(200);
+      }
+    }
+  });
 })
